e4669609182680ad3586f9a40d78e70f9eeb5fe7
lauri.aho <brilliancegenois@gmail.com>
2024-01-27 00:37:49 +0200

    slime jump and chase player haha terraria

--- slime.gd
+++ slime.gd
@@ -1,11 +1,17 @@
 extends CharacterBody2D
+const JUMP_VELOCITY = -250.0
+const JumpXVelocity = 100
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var anim = get_node("AnimationPlayer") 
 
 
 var playerIsNear
+var canSeePlayer
 var player
+var jumpSide
+var jumpCD = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player = $"../Player"
@@ -14,21 +20,39 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if(jumpCD >= 0):
+		jumpCD -= delta
+		
+	if(player.global_position.x > global_position.x):
+		jumpSide = 1
+	else:
+		jumpSide = -1
 	var space_state = get_world_2d().direct_space_state
+	
 	if not is_on_floor():
 		velocity.y += gravity * delta
-	anim.play("Jump")
+		velocity.x = JumpXVelocity * jumpSide
+	else:
+		velocity.x = 0
+
+	
 	move_and_slide()
+	
 	if(playerIsNear):
 		var visionQuery = PhysicsRayQueryParameters2D.create(global_position, player.position)
 		visionQuery.exclude = [self]
-
 		var result = space_state.intersect_ray(visionQuery)
 		if(result.size() > 0):
-			print("oof")
-			print(result.values())
-			if(result.values().has("Player")):
-				print("EEP!")
+			if(result.collider.name == "Player"):
+				canSeePlayer = true
+			else:
+				canSeePlayer = false
+				
+	if(playerIsNear && canSeePlayer && is_on_floor() && jumpCD <= 0):
+		jumpCD = 3
+		anim.play("Jump")
+		velocity.y = JUMP_VELOCITY	
+		
 
 
 func _on_detection_area_body_entered(body):
@@ -39,3 +63,4 @@ func _on_detection_area_body_entered(body):
 func _on_detection_area_body_exited(body):
 	if(body.name == "Player"):
 		playerIsNear = false
+		canSeePlayer = false
